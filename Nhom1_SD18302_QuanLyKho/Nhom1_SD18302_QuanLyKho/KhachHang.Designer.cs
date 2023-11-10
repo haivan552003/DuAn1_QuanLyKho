d8a1f8a6a04569e10e610c357636086a1c3c0275
miinhkha <miinhkha10@gmail.com>
2023-11-10 17:43:09 +0700

    tao xong giao dien from khach hang

--- Nhom1_SD18302_QuanLyKho/Nhom1_SD18302_QuanLyKho/KhachHang.Designer.cs
+++ Nhom1_SD18302_QuanLyKho/Nhom1_SD18302_QuanLyKho/KhachHang.Designer.cs
@@ -28,12 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "KhachHang";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KhachHang));
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            pictureBox2 = new PictureBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(134, 179, 209);
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.White;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(833, 677);
+            button4.Margin = new Padding(4);
+            button4.Name = "button4";
+            button4.Size = new Size(125, 56);
+            button4.TabIndex = 32;
+            button4.Text = "Làm mới";
+            button4.TextAlign = ContentAlignment.MiddleRight;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(132, 77, 54);
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(966, 677);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Padding = new Padding(6, 0, 6, 0);
+            button3.Size = new Size(125, 56);
+            button3.TabIndex = 31;
+            button3.Text = "Xóa";
+            button3.TextAlign = ContentAlignment.MiddleRight;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(134, 179, 209);
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(229, 677);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 56);
+            button2.TabIndex = 30;
+            button2.Text = "Cập nhập";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(134, 179, 209);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(96, 677);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Padding = new Padding(6, 0, 6, 0);
+            button1.Size = new Size(125, 56);
+            button1.TabIndex = 29;
+            button1.Text = "Thêm";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ActiveBorder;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(96, 162);
+            dataGridView1.Margin = new Padding(4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(995, 306);
+            dataGridView1.TabIndex = 26;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(810, 106);
+            pictureBox2.Margin = new Padding(4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(38, 38);
+            pictureBox2.TabIndex = 25;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.Location = new Point(741, 530);
+            textBox7.Margin = new Padding(4);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(349, 31);
+            textBox7.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(741, 572);
+            textBox6.Margin = new Padding(4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(349, 31);
+            textBox6.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(217, 530);
+            textBox4.Margin = new Padding(4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(349, 31);
+            textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(741, 489);
+            textBox5.Margin = new Padding(4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(349, 31);
+            textBox5.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(217, 489);
+            textBox2.Margin = new Padding(4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(349, 31);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(421, 106);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(386, 31);
+            textBox1.TabIndex = 18;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(96, 104);
+            comboBox1.Margin = new Padding(4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(186, 33);
+            comboBox1.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(170, 160, 160);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(96, 13);
+            pictureBox1.Margin = new Padding(4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(188, 62);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(96, 536);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 28);
+            label3.TabIndex = 12;
+            label3.Text = "Tên :";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(632, 577);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(80, 28);
+            label8.TabIndex = 11;
+            label8.Text = "Mã ND:";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(632, 536);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 28);
+            label7.TabIndex = 10;
+            label7.Text = "SĐT:";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(632, 489);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 28);
+            label6.TabIndex = 9;
+            label6.Text = "Email:";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(96, 489);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 28);
+            label2.TabIndex = 15;
+            label2.Text = "Mã KH:";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(491, 32);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(213, 41);
+            label1.TabIndex = 8;
+            label1.Text = "KHÁCH HÀNG";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // KhachHang
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(170, 160, 160);
+            ClientSize = new Size(1168, 753);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "KhachHang";
+            Text = "KhachHang";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox2;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label2;
+        private Label label1;
     }
 }
