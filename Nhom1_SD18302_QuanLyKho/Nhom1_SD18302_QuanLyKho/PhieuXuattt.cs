9db2af189fd4aa49cedbf3c52bb1da838d74e3ab
vunguyen1H <144969196+vunguyen1H@users.noreply.github.com>
2023-11-12 12:06:37 +0700

    frm phieu nhap,phieu xuat, nha cung cap

---
+++ Nhom1_SD18302_QuanLyKho/Nhom1_SD18302_QuanLyKho/PhieuXuattt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhom1_SD18302_QuanLyKho
+{
+    public partial class PhieuXuattt : Form
+    {
+        public PhieuXuattt()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
